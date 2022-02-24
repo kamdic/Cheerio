34ce528f36c191bb9662c381d0abfa69151bbe17
kamdic <chukk027.316@bramptonmanor.org.uk>
2022-02-24 19:37:23 +0000

    Admin User and Teams page
    The admin user can now choose the user type for a new user, and once this happens, the user is then verified. They also now have the ability to create teams. These created teams can be viewed by athletes and coaches from their pages as well.

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date, datetime
-from flask_admin.contrib.sqla import ModelView
+from datetime import datetime
 from flask_login import UserMixin, login_user, login_required, logout_user, LoginManager, current_user
 from werkzeug.utils import redirect
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_admin import Admin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cheer.db'
@@ -19,18 +17,16 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Users.query.get(user_id)
+    return User.query.get(user_id)
 
 
-class Users(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     firstName = db.Column(db.String(50),nullable = False)
     surname = db.Column(db.String(50), nullable = False)
-    
-    #dob = db.Column(db.DateTime, nullable = True)    
+    dob = db.Column(db.DateTime, nullable = False)    
     school_URN = db.Column(db.Integer, nullable = True)
-    #start_date = db.Column(db.DateTime, nullable = True, default=datetime.utcnow)
-    
+    start_date = db.Column(db.DateTime, nullable = True, default=datetime.utcnow)
     email = db.Column(db.String(70), nullable = False)
     password = db.Column(db.String(255), nullable = False)
     usertype = db.Column(db.Integer, default = -1)
@@ -44,22 +40,26 @@ class Fees(db.Model):
     amount = db.Column(db.Float, nullable = False)
     paid_date = db.Column(db.DateTime, nullable = False, default= 'N/A')
 
-class Teams(db.Model):
+class Team_Details(db.Model):
     team_id = db.Column(db.Integer, primary_key= True)
     coach_id = db.Column(db.Integer)
-    athlete_id = db.Column(db.Integer)
     team_name= db.Column(db.String(20), nullable= False)
     max_age = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
         return 'Team' +str(self.team_id)
 
+class Team_Events(db.Model):
+    team_sheet_id= db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, primary_key = True)
+    team_id = db.Column(db.Integer, primary_key= True)
+    section = db.Column(db.Integer)
+    #section = {0: opening, 1: stunt 1, 2:jumps, 3:baskets, 4:stunt 2, 5:pyramid 6:tumble run 7:dance}
+
 class Team_Sheet(db.Model):
     team_sheet_id= db.Column(db.Integer, primary_key=True)
-    team_id= db.Column(db.Integer)
-    athlete_id= db.Column(db.Integer)
+    athlete_id= db.Column(db.Integer, primary_key= True)
     role= db.Column(db.String(10), default = 'Tumbler')
-    
     
 class Training(db.Model):
     training_id= db.Column(db.Integer, primary_key=True)
@@ -82,9 +82,6 @@ class Events(db.Model):
     event_name= db.Column(db.Text, nullable =False)
     event_start_date= db.Column(db.Date, nullable = False)
 
-# admin = Admin(app)
-# admin.add_view(ModelView(Users, db.session))
-# admin.add_view(ModelView(Teams, db.session))
 
 @app.route('/')
 def index():
@@ -93,17 +90,15 @@ def index():
 @app.route('/signup',  methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        data=request.form
         email = request.form.get('email')
         firstName = request.form.get('firstName')
         surname = request.form.get('surname')
         dob = request.form.get("dob")
         school_URN= request.form.get("school_URN")
         start_date= request.form.get("start_date")
-        usertype= request.form.get("usertype")
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        user = Users.query.filter_by(email=email).first()
+        user = User.query.filter_by(email=email).first()
         if user:
             flash('There is already an account using this email', category='error')
         elif len(email) <= 3:
@@ -111,11 +106,12 @@ def signup():
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         else:
-            new_user = Users(email=email, firstName=firstName, surname=surname, school_URN=school_URN, password=generate_password_hash(password1, method='sha256'))
+            dob = dob.split("-")
+            new_user = User(email=email, firstName=firstName, surname=surname, school_URN=school_URN, dob = datetime(int(dob[0]), int(dob[1]), int(dob[2])), start_date=start_date, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Your account has been created. Wait for the admin user to verify it.', category='success')
-            return redirect(url_for('index'))
+            return redirect(url_for('login'))
         
     return render_template('signup.html')
 
@@ -124,7 +120,7 @@ def login():
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
-        user = Users.query.filter_by(email=email).first()
+        user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
                 if user.verified == 1:
@@ -153,14 +149,62 @@ def logout():
 def admin():
     return render_template('admin.html')
 
-@app.route('/users')
+@app.route('/users', methods= ['GET', 'POST'])
 @login_required
 def users():
     if current_user.usertype < 4:
         return "You are not authorised to view this page"
-
-    query = db.engine.execute("SELECT id, firstName, surname, email FROM users WHERE verified < 1")  
+    query = User.query.filter(User.verified < 1).all()
+    valid = False
+    for item in request.values:
+        if "_" in item:
+            items = item.split("_")
+            if len(items) == 2 and items[0].lower() == "usertype":
+                try:
+                    int(items[1])
+                    valid = item
+                except:
+                    continue
+    if valid:
+        usertype = request.form.get(valid)
+        userid = valid.split("_")[1]
+        if request.method == 'POST':
+            user = User.query.filter_by(id = userid).first()
+            user.usertype = usertype     
+            user.verified = 1
+            db.session.merge(user)
+            db.session.commit()
+            return redirect(url_for('users')) 
     return render_template('users.html', users=query)
+
+@app.route('/teams', methods=['GET','POST'])
+@login_required
+def teams():
+    if current_user.usertype < 4:
+        query = db.engine.execute("SELECT * FROM team__details")
+        names = db.engine.execute("SELECT user.firstName, user.surname FROM user INNER JOIN team__details ON user.id = team__details.coach_id")
+        max_ages = []
+        team_names = []
+        for team in query:
+            team_names.append(team[2])
+            max_ages.append(team[3])
+        coach_names = [(name[0] + " " + name[1]) for name in names]
+        team_num = len(team_names)
+        return render_template('teams.html', team_names = team_names, max_ages= max_ages, coach_names = coach_names, team_num = team_num)
+    
+    query = User.query.filter(User.usertype > 0)
+    athletes = User.query.filter(User.usertype >-1)
+    if request.method == 'POST':
+        coach_id = request.form.get('coach')
+        team_name = request.form.get('team_name')
+        max_age = request.form.get('max_age')
+        new_team = Team_Details(coach_id= coach_id, team_name= team_name, max_age= max_age)
+        db.session.add(new_team)
+        db.session.commit()
+        flash('Your new team: {} has been created.'.format(team_name), category='success')
+        return redirect(url_for('teams'))
+    return render_template('createteams.html', coaches = query, athletes= athletes)
+
 
 @app.route('/events')
 @login_required
@@ -171,22 +215,6 @@ def events():
 @login_required
 def fees():
     return render_template('fees.html')
-
-@app.route('/teams', methods=['GET','POST'])
-@login_required
-def teams():
-    if request.method == 'POST':
-        team_name= request.form['team_name']
-        team_id= request.form['team_id']
-        coach_id= request.form['coach_id']
-        athlete_id= request.form['athlete_id']
-        max_age= request.form['max_age']
-        new_team = Teams(team_id=team_id, coach_id=coach_id, athlete_id=athlete_id, team_name=team_name, max_age=max_age)
-        db.session.add(new_team)
-        db.session.commit()
-        return redirect('/teams')
-    else:
-       return render_template('teams.html', teams=Teams.query.all(), title="Show all Teams")
 
 @app.route('/admin/training')
 def manage_training():
